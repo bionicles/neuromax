a440e44fb4737c8812e2e41eba07e218f7312e12
unknown <kamelbelkadhi2@gmail.com>
2019-06-03 13:26:38 +0200

    Add dropout layer, fix some code errors. Looking how to rely model blocks in a dynamic way.

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
 # model.py
 # why? run the model on the input
 
-from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras import Model
 
 
 # we make one block in a res-net
-def make_block(tensor, config):
-    output = Dense(tensor, activation=config.ACTIVATION)
-    for layer_num in config.NUM_LAYERS - 1:
-        output = Dense(output, activation=config.ACTIVATION)
+def make_block(input_layer, config):
+    output = Dense(units = config.UNITS, activation=config.ACTIVATION)(input_layer)
+    for layer_num in range(config.NUM_LAYERS - 1):
+        output = Dense(units = config.UNITS, activation=config.ACTIVATION)(output)
+        if config.DROPOUT: #True or False
+        	output = Dropout(rate = config.DROPOUT_RATE)(output)
     return output
 
 
@@ -18,5 +20,6 @@ def make_model(config):
     input = Input(shape=(1, None, config.ATOM_DIMENSION))
     output = make_block(input, config)
     for i in range(config.NUM_BLOCKS):
-        output += make_block(out, config)
-    return Model(input, output)
+        output += make_block(output, config)
+    model_output = Dense(units = config.OUTPUT_SHAPE, activation = config.ACTIVATION)(output) 
+    return Model(input, model_output)
