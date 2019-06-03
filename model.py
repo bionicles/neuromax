6eaead5209d490c0666744c0d618debe6cc82286
unknown <kamelbelkadhi2@gmail.com>
2019-06-03 14:18:52 +0200

    Create dynamic layers name. Looking how to call them later for the merge layer.

--- model.py
+++ model.py
@@ -1,25 +1,27 @@
 # model.py
 # why? run the model on the input
 
-from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.layers import Input, Dense, Dropout, average, Layer
 from tensorflow.keras import Model
 
 
 # we make one block in a res-net
-def make_block(input_layer, config):
+def make_block(input_layer, config, block_num):
     output = Dense(units = config.UNITS, activation=config.ACTIVATION)(input_layer)
     for layer_num in range(config.NUM_LAYERS - 1):
-        output = Dense(units = config.UNITS, activation=config.ACTIVATION)(output)
+        output = Dense(units = config.UNITS, activation=config.ACTIVATION, name = 'block_{}'.format(block_num))(output)
         if config.DROPOUT: #True or False
-        	output = Dropout(rate = config.DROPOUT_RATE)(output)
+        	output = Dropout(rate = config.DROPOUT_RATE)(output) # not sure if dropout layer support names option
+    if block_num%config.CONNECT_BLOCK_AT == 0: #exp: each four separated blocks connect them through average/add/dot... layer
+    	output = average([output, ' how to call layers by their name for averaging'])
     return output
 
 
 # we make a model
 def make_model(config):
     input = Input(shape=(1, None, config.ATOM_DIMENSION))
-    output = make_block(input, config)
-    for i in range(config.NUM_BLOCKS):
-        output += make_block(output, config)
+    output = make_block(input, config, block_num = 0) # initial block
+    for i in range(1, config.NUM_BLOCKS + 1): #start count blocks from 1, useful to connect blocks using their numbers
+        output = make_block(output, config, block_num)
     model_output = Dense(units = config.OUTPUT_SHAPE, activation = config.ACTIVATION)(output) 
     return Model(input, model_output)
