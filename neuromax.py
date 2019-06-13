13a72d444246637653fac3fc464f47acbd5db8b5
Bion Howard <bion@bitpharma.com>
2019-06-12 23:23:06 -0400

    somewhat working

--- neuromax.py
+++ neuromax.py
@@ -15,7 +15,8 @@ import time
 import csv
 import os
 
-N_BLOCKS = 1
+tf.enable_eager_execution()
+N_BLOCKS = 0
 
 
 def make_block(block_inputs, MaybeNoiseOrOutput):
@@ -50,22 +51,24 @@ def make_agent():
 initial_positions, velocity_zero, transpose_mass, masses = [], [], [], []
 velocities, positions, chains, model = [], [], [], []
 episode_stacks_path, episode_pngs_path = '', ''
-TIME = str(time.time())
-ROOT = os.path.abspath('.')
 episode, step, num_atoms = 0, 0, 0
+ROOT = os.path.abspath('.')
+TIME = str(time.time())
 max_work = np.inf
 pdb_name = ''
 # task
-MAX_UNDOCK_DISTANCE = 100
-MIN_UNDOCK_DISTANCE = 10
-MAX_STEPS_IN_UNDOCK = 1
-MIN_STEPS_IN_UNDOCK = 1
+MAX_UNDOCK_DISTANCE = 8
+MIN_UNDOCK_DISTANCE = 4
+MAX_STEPS_IN_UNDOCK = 5
+MIN_STEPS_IN_UNDOCK = 5
+MAX_STEPS_IN_UNFOLD = 0
+MIN_STEPS_IN_UNFOLD = 0
 STOP_LOSS_MULTIPLE = 10
-SCREENSHOT_EVERY = 20
-NUM_EPISODES = 10000
-NUM_FEATURES = 17
+SCREENSHOT_EVERY = 2
 IMAGE_SIZE = 256
-ATOM_JIGGLE = 1
+NUM_EPISODES = 1
+NUM_STEPS = 10
+BUFFER = 64
 
 
 def load_pedagogy():
@@ -84,12 +87,11 @@ def prepare_pymol():
     cmd.remove('solvent')
     color_chainbow()
     cmd.set('depth_cue', 0)
-    cmd.unpick()
     cmd.show('surface')
-    cmd.zoom('all', buffer=42, state=-1)
+    cmd.zoom('all', buffer=BUFFER, state=-1)
     cmd.center('all')
     cmd.bg_color('black')
-    cmd.deselect()
+    # cmd.deselect()
 
 
 def pick_colors(number_of_colors):
@@ -108,10 +110,10 @@ def make_gif():
     gif_path = os.path.join(ROOT, 'gifs', gif_name)
     imagepaths, images = [], []
     for stackname in os.listdir(episode_stacks_path):
+        print("processing", stackname)
         filepath = os.path.join(episode_stacks_path, stackname)
         imagepaths.append(filepath)
     imagepaths.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-    del imagepaths[0]  # delete stacked images folder from the list
     for imagepath in imagepaths:
         image = imageio.imread(imagepath)
         images.append(image)
@@ -120,17 +122,17 @@ def make_gif():
 
 
 def make_image():
-    step_string = 'e-{}_p-{}_s-{}'.format(episode, pdb_name, step)
+    step_string = str(step)
     png_paths = take_pictures(step_string)
     images = [np.asarray(Image.open(png)) for png in png_paths]
     vstack = np.vstack(images)
     image_path = os.path.join(episode_stacks_path, step_string + '.png')
     vstack_img = Image.fromarray(vstack)
+    print('screenshot', image_path)
     vstack_img.save(image_path)
 
 
 def take_pictures(step_string):
-    print('PNGS', TIME, 'episode', episode, 'pdb', pdb_name, 'step', step)
     cmd.deselect()
     png_path_x = os.path.join(episode_pngs_path, step_string + '-X.png')
     cmd.png(png_path_x, width=IMAGE_SIZE, height=IMAGE_SIZE)
@@ -151,7 +153,6 @@ def take_pictures(step_string):
 def get_positions():
     model = cmd.get_model('all', 1)
     positions = np.array(model.get_coord_list())
-    print(type(positions), positions)
     return positions
 
 
@@ -169,18 +170,15 @@ def get_atom_features(atom):
                      sum([ord(i) for i in atom.symbol])//len(atom.symbol)])
 
 
-def calculate_work():
-    print("positions.size")
-    print(type(positions), positions)
-    print("initial_positions.size")
-    print(type(initial_positions), initial_positions, initial_positions.size)
+def calculate_work(p, v):
+    p = tf.squeeze(p).numpy()
     # work to move atoms back to initial positions
-    distance = cdist(positions, initial_positions)
+    distance = cdist(p, initial_positions)
     work = distance * transpose_mass
-    work = np.triu(work, 1)  # k=1 should zero main diagonal
+    work = np.tril(work, -1)  # k=1 should zero main diagonal
     work = np.sum(work)
     # work to slow atoms to a stop
-    work_to_stop = np.transpose(velocities) * transpose_mass
+    work_to_stop = np.transpose(v) * transpose_mass
     work_to_stop = np.triu(work_to_stop, 1)
     work_to_stop = np.sum(work_to_stop)
     if work_to_stop > 0:
@@ -188,12 +186,12 @@ def calculate_work():
     return work
 
 
-def undock(screenshot, chains):
+def undock():
     global step
     steps_in_undock = random.randint(MIN_STEPS_IN_UNDOCK, MAX_STEPS_IN_UNDOCK)
     print('undocking', pdb_name, steps_in_undock, 'times')
     step_vector_array, sum_vector = [], {}
-    for step in range(steps_in_undock):
+    for undock_step in range(steps_in_undock):
         current_step_vectors = []
         for chain in chains:  # x, y, z, rx, ry, rz
             vector = np.array([
@@ -208,40 +206,42 @@ def undock(screenshot, chains):
                 sum_vector[chain] = vector
             else:
                 sum_vector[chain] = sum_vector[chain] + vector
-            # save the components also
             current_step_vectors.append(vector)
         step_vector_array.append(current_step_vectors)
     # move to the final position
     for chain in chains:
         if chain in sum_vector.keys():
-            selection_string = 'current and chain ' + chain
+            selection_string = 'chain ' + chain
             vector = sum_vector[chain]
+            print("moving chains out!", chain, vector, type(vector))
             final_translation_vector = list(vector[:3])
             cmd.translate(final_translation_vector, selection_string)
             cmd.rotate('x', vector[3], selection_string)
             cmd.rotate('y', vector[4], selection_string)
             cmd.rotate('z', vector[5], selection_string)
     # set the zoom on the final destination
-    if screenshot:
-        prepare_pymol()
+    prepare_pymol()
     # prepare to animate undock by moving back to original position
     for chain in chains:
         if chain in sum_vector.keys():
+            selection_string = 'chain ' + chain
             vector = sum_vector[chain] * -1  # move back
             inverse_translation_vector = list(vector[:3])
-            cmd.translate(inverse_translation_vector, 'chain ' + chain)
-            cmd.rotate('x', vector[3], 'current and chain ' + chain)
-            cmd.rotate('y', vector[4], 'current and chain ' + chain)
-            cmd.rotate('z', vector[5], 'current and chain ' + chain)
+            print("moving chains back!", chain, inverse_translation_vector, type(inverse_translation_vector))
+            cmd.translate(inverse_translation_vector, selection_string)
+            cmd.rotate('x', vector[3], selection_string)
+            cmd.rotate('y', vector[4], selection_string)
+            cmd.rotate('z', vector[5], selection_string)
     # move through the list of steps, move the chains, take the screenshots
     if screenshot:
         make_image()
-    step = 1
+    step += 1
     for step_vector in step_vector_array:
         for k in range(len(step_vector)):
             chain_vector = step_vector[k]
             chain = chains[k]
             current_vector = list(chain_vector[:3])
+            print("moving chains out!", chain, current_vector, type(current_vector))
             cmd.translate(current_vector, 'current and chain ' + chain)
             cmd.rotate('x', chain_vector[3], 'current and chain ' + chain)
             cmd.rotate('y', chain_vector[4], 'current and chain ' + chain)
@@ -251,19 +251,18 @@ def undock(screenshot, chains):
         step += 1
 
 
-def unfold(screenshot, chains):
+def unfold():
     global step
-    num_steps = random.randint(MIN_STEPS_IN_UNDOCK, MAX_STEPS_IN_UNDOCK)
+    num_steps = random.randint(MIN_STEPS_IN_UNFOLD, MAX_STEPS_IN_UNFOLD)
     print('unfolding', pdb_name, num_steps, 'times')
     for fold_step in range(num_steps):
         for chain in chains:
-            np.array([
-                unfold_index(name, index) for name, index in
-                cmd.index('byca (chain {})'.format(chain))])
-        cmd.deselect()
+            np.array([unfold_index(name, index) for name, index in
+                      cmd.index('byca (chain {})'.format(chain))])
         if screenshot:
+            cmd.unpick()
             make_image()
-    step += num_steps
+        step += 1
 
 
 def unfold_index(name, index):
@@ -318,35 +317,34 @@ def reset():
     model = cmd.get_model('current', 1)
     features = np.array([get_atom_features(atom) for atom in model.atom])
     mass = np.array([atom.get_mass() for atom in model.atom])
-    masses = tf.cast(tf.stack([mass, mass, mass], 1), tf.float32)
-    print("masses", masses)
-    transpose_mass = tf.cast(tf.transpose(mass), tf.float32)
-    print("transpose_mass", transpose_mass)
-    undock(screenshot, chains)
-    unfold(screenshot, chains)
+    masses = np.float32(np.stack([mass, mass, mass], 1))
+    transpose_mass = np.float32(np.transpose(mass))
+    undock()
+    unfold()
     positions = get_positions()
-    initial_work = calculate_work()
+    initial_work = calculate_work(positions, velocities)
     max_work = initial_work * STOP_LOSS_MULTIPLE
     return positions, velocities, features
 
 
 def move_atom(xyz, atom_index):
     atom_selection_string = 'id ' + str(atom_index)
-    cmd.translate(list(xyz), atom_selection_string)
+    xyz = list(xyz)
+    cmd.translate(xyz, atom_selection_string)
     atom_index += 1
 
 
 def move_atoms(potentials):
     force = -1 * potentials
     acceleration = force / masses
-    noise = np.random.normal(loc=0.0, scale=ATOM_JIGGLE, size=potentials.shape)
-    new_velocities = velocities + acceleration + noise
+    noise = random_normal(potentials.shape)
+    new_velocities = tf.add_n([velocities, acceleration, noise])
+    new_velocities = tf.squeeze(new_velocities)
+    print("new_velocities size", new_velocities.shape)
     atom_index = 0
     np.array([move_atom(xyz, atom_index) for xyz in new_velocities])
     new_positions = positions + new_velocities
-    work = calculate_work()
-    done = work > max_work
-    return new_positions, new_velocities, work, done
+    return new_positions, tf.expand_dims(new_velocities, 0)
 
 
 class AttrDict(dict):
@@ -355,8 +353,8 @@ class AttrDict(dict):
 
 
 def train():
-    global screenshot, positions, velocities, features, episode, step
-    callbacks = [TensorBoard(), ReduceLROnPlateau(monitor='loss')]
+    global screenshot, positions, velocities, features, episode, step, work
+    # callbacks = [TensorBoard(), ReduceLROnPlateau(monitor='loss')]
     predictor, actor, critic = make_agent()
     predictor.compile(loss='mse', optimizer='nadam')
     critic.compile(loss='mse', optimizer='nadam')
@@ -365,27 +363,29 @@ def train():
     load_pedagogy()
     for i in range(NUM_EPISODES):
         screenshot = episode % SCREENSHOT_EVERY == 0
+        done, step = False, 0
         reset()
         positions = np.expand_dims(np.float32(positions), 0)
         velocities = np.expand_dims(np.float32(velocities), 0)
         features = np.expand_dims(np.float32(features), 0)
-        done, step = False, 0
         while not done:
-            print("predictor call")
             atoms = tf.concat([positions, velocities, features], 2)
             p = random_normal(shape=(1, num_atoms, 6))
             prediction = predictor([atoms, p])
-            print("actor call")
             a_in = tf.concat([atoms, prediction], 2)
             a = random_normal(shape=(1, num_atoms, 3))
             action = actor([a_in, a])
-            print("critic call")
             c_in = tf.concat([atoms, prediction, action], 2)
             c = random_normal(shape=(1, num_atoms, 1))
             criticism = critic([c_in, c])
-            new_positions, new_velocities, work, done = move_atoms(action)
-            memory.append(AttrDict({'new_velocities': new_velocities,
-                                    'new_positions': new_positions,
+            new_positions, new_velocities = move_atoms(action)
+            work = calculate_work(new_positions, new_velocities)
+            too_much_work = work > max_work
+            too_many_steps = step > NUM_STEPS
+            done = too_much_work or too_many_steps
+            make_image()
+            memory.append(AttrDict({'new_v': new_velocities,
+                                    'new_p': new_positions,
                                     'prediction': prediction,
                                     'velocities': velocities,
                                     'positions': positions,
@@ -394,19 +394,18 @@ def train():
                                     'work': work}))
             velocities, positions = new_velocities, new_positions
             step += 1
-        make_gif()
-        zero_velocities = np.zeros_like(velocities)
-        action_target = tf.concat([initial_positions, zero_velocities], 2)
-        for event in memory:  # optimize
-            action_result = tf.concat(
-                                [event.new_positions, event.new_velocities], 2)
-            future = tf.concat([event.new_positions, event.new_velocities], 2)
-            actor.fit(action_result, action_target, callbacks=callbacks)
-            predictor.fit(event.prediction, future, callbacks=callbacks)
-            critic.fit(event.criticism, event.work, callback=callbacks)
-        predictor.save_model()
-        critic.save_model()
-        actor.save_model()
+        if screenshot:
+            make_gif()
+        # zero_velocities = np.zeros_like(velocities)
+        # action_target = tf.concat([initial_positions, zero_velocities], 2)
+        # for event in memory:  # optimize
+        #     future = tf.concat([event.new_p, event.new_v], 2)
+        #     predictor.fit(event.prediction, future, callbacks=callbacks)
+        #     actor.fit(future, action_target, callbacks=callbacks)
+        #     critic.fit(event.criticism, event.work, callback=callbacks)
+        # predictor.save_model()
+        # critic.save_model()
+        # actor.save_model()
         episode += 1
 
 
