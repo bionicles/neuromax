54322937eaf9a6cab1ddd0643710532892f71024
unknown <kamelbelkadhi2@gmail.com>
2019-06-16 00:27:25 +0200

    Fix make_block inputs.

--- resnet.py
+++ resnet.py
@@ -6,6 +6,18 @@ from tensorflow.keras import Model
 #https://ray.readthedocs.io/en/latest/rllib-models.html
 
 class ResNet(Model):
+            """Define the layers of a custom model.
+
+        Arguments:
+            input_dict (dict): Dictionary of input tensors, including "obs",
+                "prev_action", "prev_reward", "is_training".
+            num_outputs (int): Output tensor must be of size
+                [BATCH_SIZE, num_outputs].
+            options (dict): Model options.
+
+        Returns:
+            (outputs, feature_layer): Tensors of size [BATCH_SIZE, num_outputs]
+                and [BATCH_SIZE, desired_feature_size]"""
     def __init__(self, name, in1, in2, layers, units, blocks, gain, l1, l2):
         self.name = name
         self.in1 = in1
@@ -16,15 +28,15 @@ class ResNet(Model):
         self.gain = gain
         self.l1 = l1
         self.l2 = l2
-    def make_block(self, features, noise, layers, units, gain, l1, l2):
+    def make_block(self, features, noise):
         Attention_layer = Attention()([features, features])
         block_output = Concatenate(2)([Attention_layer, MaybeNoiseOrOutput])
         block_output = Activation('tanh')(block_output)
-        for layer_number in range(0, round(layers.item())-1):
-            block_output = Dense(units,
-                                 kernel_initializer=Orthogonal(gain),
-                                 kernel_regularizer=L1L2(l1, l2),
-                                 bias_regularizer=L1L2(l1, l2),
+        for layer_number in range(0, round(self.layers.item())-1):
+            block_output = Dense(self.units,
+                                 kernel_initializer=Orthogonal(self.gain),
+                                 kernel_regularizer=L1L2(self.l1, self.l2),
+                                 bias_regularizer=L1L2(self.l1,self. l2),
                                  activation='tanh'
                                  )(block_output)
             block_output = Dropout(0.5)(block_output)
