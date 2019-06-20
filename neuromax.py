9cc5dcb14005b8251b42b0ff8afce66dff0afd6e
Bion Howard <bion@bitpharma.com>
2019-06-20 12:10:25 -0400

    simplify parse_example

--- neuromax.py
+++ neuromax.py
@@ -1,10 +1,12 @@
 # neuromax.py - why?: 1 simple file with functions over classes
+import tensorflow as tf
+tf.compat.v1.enable_eager_execution()
+
 from bayes_opt import BayesianOptimization
 from bayes_opt.observer import JSONLogger
 from bayes_opt.util import load_logs
 from bayes_opt.event import Events
 from functools import partial
-import tensorflow as tf
 import numpy as np
 import imageio
 import random
@@ -50,12 +52,68 @@ pbounds = {
     'EPSILON': (1e-4, 1),
 }
 
+# begin model
+from tensorflow.keras.layers import Layer, Dense, Concatenate
+from tensorflow.keras.initializers import Orthogonal
+from tensorflow.keras import Model, Input
+import tensorflow as tf
+
+
+def get_mlp(features, outputs, units_array):
+    input = Input((features * 2))
+    output = Dense(units, activation='tanh', kernel_initializer=Orthogonal)(input)
+    for units in units_array[1:]:
+        output = Dense(units, activation='tanh', kernel_initializer=Orthogonal)(output)
+    output = Dense(outputs, activation='tanh', kernel_initializer=Orthogonal)(output)
+    return Model(input, output)
+
+
+class ConvPair(Layer):
+    def __init__(self,
+                 units_array=[2048, 2048],
+                 features=16,
+                 outputs=3):
+        super(ConvPair, self).__init__()
+        self.kernel = get_mlp(features, outputs, units_array)
+
+    def __call__(self, inputs):
+        return tf.py_function(func=self.op, inp=inputs, Tout=tf.float32)
+
+    def op(self, inputs):
+        outputs = tf.zeros((inputs.shape[0], self.units_array[-1]))
+        for i in num_atoms:
+            for j in inputs:
+                if i == j:
+                    continue
+                else:
+                    k_in = tf.concat([inputs[i,:], inputs[j,:]])
+                    outputs[i] = self.kernel(k_in)
+        return outputs
+
+
+def make_block(features, noise_or_output, n_layers):
+    block_output = Concatenate(2)([features, noise_or_output])
+    for layer_n in range(0, n_layers - 1):
+        block_output = ConvPair()(block_output)
+        block_output = Concatenate(2)([features, block_output])
+    block_output = ConvPair()(block_output)
+    block_output = Add()([block_output, noise_or_output])
+    return block_output
+
+
+def make_resnet(name, d_in, d_out, blocks, layers):
+    features = Input((None, d_in))
+    noise = Input((None, d_out))
+    output = make_block(features, noise, layers)
+    for i in range(1, round(blocks.item())):
+        output = make_block(features, noise, layers)
+    output *= -1
+    return Model([features, noise], output)
+# end model
+
 # begin dataset
 def parse_example(example):
-    context_features={
-        'num_atoms': tf.io.FixedLenFeature([], dtype=tf.int64),  # scalar
-        'protein': tf.io.FixedLenFeature([], dtype=tf.string),  # scalar
-    }
+    context_features={'protein': tf.io.FixedLenFeature([], dtype=tf.string)}
     sequence_features={
         'initial_positions': tf.io.FixedLenSequenceFeature([], dtype=tf.string),  # num_atoms * 3
         'positions': tf.io.FixedLenSequenceFeature([], dtype=tf.string),  # num_atoms * 3
@@ -66,17 +124,13 @@ def parse_example(example):
     initial_positions = parse_feature(sequence['initial_positions'][0], 3)
     features = parse_feature(sequence['features'][0], 9)
     masses = parse_feature(sequence['masses'][0], 1)
-    features = tf.concat([masses, features], 1)
     return {
         'initial_positions': initial_positions,
         'initial_distances': calculate_distances(initial_positions),
-        'protein': context['protein'].numpy().decode(),
-        'num_atoms': num_atoms,
-        'num_atoms_squared': num_atoms ** 2,
+        'protein_tensor': context['protein'],
         'positions': parse_feature(sequence['positions'][0], 3),
-        'velocities': random_normal((num_atoms, 3), 0, NOISE, dtype='float16'),
-        'masses': tf.concat([masses, masses, masses], 2),
-        'features': features,
+        'masses': tf.concat([masses, masses, masses], 1),
+        'features': tf.concat([masses, features], 1),
         }
 
 
@@ -219,7 +273,7 @@ def trial(BLOCKS, LAYERS, LR, EPSILON):
 
 def main():
     global NUM_EPISODES, experiment, save_model
-    tf.enable_eager_execution()
+    print('eager?', tf.executing_eagerly())
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     logger = JSONLogger(path='./runs/logs.json')
     bayes = BayesianOptimization(f=partial(trial),
