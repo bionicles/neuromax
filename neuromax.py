87bc419c4b08442bca25bdd235fd8541465630e0
Bion Howard <bion@bitpharma.com>
2019-06-28 04:36:47 -0400

    fix escape hatch

--- neuromax.py
+++ neuromax.py
@@ -248,7 +248,7 @@ def trial(**kwargs):
             ema.apply(agent.weights)
             loss = tf.reduce_sum(position_loss) + tf.reduce_sum(shape_loss)
             tf.print(step, 'stop', stop, 'loss', loss)
-            if tf.math.greater(loss, stop) or tf.math.is_nan(loss):
+            if tf.math.logical_or(tf.math.greater(loss, stop), tf.math.is_nan(loss)):
                 break
             new_stop = loss * STOP_LOSS_MULTIPLE
             if tf.math.less(new_stop, stop):
