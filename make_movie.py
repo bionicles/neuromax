897d408fe4fdbf2e912272b5b85c3a291a46b9dc
kamel <kamel@bitpharma.com>
2019-06-30 23:23:21 +0100

    Generate_movie complete code

--- make_movie.py
+++ make_movie.py
@@ -1,14 +1,47 @@
+import tensorflow as tf
 from pymol import cmd, util, movie
 import random
-import tensorflow as tf
 import numpy as np
 axis = ['x', 'y', 'z']
 
-def moves_atom(xyz):
+def move_atom(xyz):
     atom_selection_string = 'id ' + str(atom_index)
     xyz = xyz.numpy().tolist()
     cmd.translate(xyz, atom_selection_string)
     atom_index += 1
+
+def translate(force):
+    model = cmd.get_model('current', 1)
+    masses = np.array([atom.get_mass() for atom in model.atom])
+    velocity = force / masses
+    atom_index = 0
+    vectors = np.array([self.translate_atom(xyz) for xyz in velocity])
+
+def translate_atom(vector):
+    # print("translate_atom", vector, type(vector), vector.shape)
+    movement_vector = list(vector) # list(movement_vector)
+    atom_selection_string = "id " + str(atom_index)
+    cmd.translate(movement_vector, atom_selection_string)
+    atom_index += 1
+
+def get_positions():
+    model = cmd.get_model('all', 1)
+    positions = np.array(model.get_coord_list())
+    return tf.convert_to_tensor(positions, preferred_dtype=DTYPE)
+
+def get_atom_features(atom):
+    return np.array([ord(atom.chain.lower())/122,
+                     atom.formal_charge,
+                     atom.partial_charge,
+                     atom.vdw,
+                     atom.b,
+                     atom.q,
+                     atom.get_free_valence(0),
+                     atom.resi,
+                     atom.index,
+                     sum([ord(i)/122 for i in atom.resn]) / len(atom.resn),
+                     sum([ord(i)/122 for i in atom.symbol]) / len(atom.symbol),
+                     sum([ord(i)/122 for i in atom.name]) / len(atom.name)])
 
 def prepare_pymol(pdb_name):
     # setup PyMOL for movies
@@ -18,7 +51,10 @@ def prepare_pymol(pdb_name):
     cmd.set('scene_buttons', 1)
     cmd.set('cache_frames', 1)
     cmd.config_mouse('three_button_motions', 1)
-    cmd.load(pdb_name+'.pdb')
+    if not os.path.exists("pdbs/" + pdb_name):
+        print('fetching', protein)
+        cmd.fetch(protein, path=os.path.join('.', 'pdbs'), type='pdb')
+    cmd.load("pdbs/" + pdb_name+'.pdb')
     cmd.remove("solvent")
     util.cbc()
     cmd.set('max_threads', 16)
@@ -75,9 +111,16 @@ def generate_movie(length, movie_name, pdb_name, agent, start_after = 1):
     cmd.frame(30*2)
     unfold()
     cmd.mview('store', object='all')
-    done = False
-    while not done:
-        forces = agent(pdb_featurs)
+    max_steps = 5
+    step = 0
+    features = np.array([get_atom_features(atom) for atom in model.atom])
+    while (step<max_steps):
+        positions = get_positions()
+        stacked_features = tf.concat([positions, features], axis=-1)
+        compressed_noise = tf.random.truncated_normal((1, tf.shape(positions)[1], 5), stddev=0.0001)
+        output_noise = tf.random.truncated_normal(tf.shape(positions), stddev=0.0001)
+        forces = agent([stacked_features, compressed_noise, output_noise])
         cmd.frame(30*length)
-        move_atoms(new_positions)
+        translate(forces)
+        step += 1
     movie.produce(filename = movie_name+'.mpg')
