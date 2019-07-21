0523c6113c63b3b21e93a6f620613b4e815a6946
Bion Howard <bion@bitpharma.com>
2019-07-21 14:36:44 -0400

    add recurse and simplify names, it passed the test

--- test_design.py
+++ test_design.py
@@ -4,7 +4,11 @@ import random
 
 B, L, K = tf.keras.backend, tf.keras.layers, tf.keras
 
-STEPS, MIN_LAYERS, MAX_LAYERS, MIN_NODES, MAX_NODES = 2, 1, 3, 1, 2
+MIN_LAYERS, MAX_LAYERS = 1, 3
+MIN_NODES, MAX_NODES = 1, 3
+P_INSERT = 0.64
+STEPS = 2
+
 IMAGE_SIZE = "1024x512"
 DTYPE = tf.float32
 
@@ -45,81 +49,90 @@ def screenshot(G, step):
 
 def get_regulon(parent=None, layers=None):
     num_layers = random.randint(MIN_LAYERS, MAX_LAYERS)
-    G = nx.MultiDiGraph()
-    names = []
+    M = nx.MultiDiGraph()
+    ids = []
     for layer_number in range(num_layers):
-        num_nodes = random.randint(MIN_NODES, MAX_NODES)
-        print("layer number", layer_number, "has", num_nodes, "nodes")
-        names_of_nodes_in_this_layer = []
-        for node_number in range(num_nodes):
-            if parent is not None:
-                node_name = "{}.{}.{}".format(parent, layer_number, node_number)
+        n_nodes = random.randint(MIN_NODES, MAX_NODES)
+        print(f"add layer {layer_number} with {n_nodes} nodes")
+        ids_of_nodes_in_this_layer = []
+        for node_number in range(n_nodes):
+            if parent:
+                node_id = "{}.{}.{}".format(parent, layer_number, node_number)
             else:
-                node_name = "{}.{}".format(layer_number, node_number)
-            print("add node", node_name)
-            G.add_node(node_name, label="", style="filled", shape="square", color="black")
-            names_of_nodes_in_this_layer.append(node_name)
-        names.append(names_of_nodes_in_this_layer)
-    for predecessor_layer_number, predecessor_node_names in enumerate(names):
-        for successor_layer_number, successor_node_names in enumerate(names):
+                node_id = "{}.{}".format(layer_number, node_number)
+            print(f"add node {node_id}")
+            M.add_node(node_id, label="", style="filled", shape="square", color="black")
+            ids_of_nodes_in_this_layer.append(node_id)
+        ids.append(ids_of_nodes_in_this_layer)
+    for predecessor_layer_number, predecessor_node_ids in enumerate(ids):
+        for successor_layer_number, successor_node_ids in enumerate(ids):
             if predecessor_layer_number >= successor_layer_number:
                 continue
-            print("predecessor", predecessor_layer_number, predecessor_node_names)
-            print("successor", successor_layer_number, successor_node_names)
-            for predecessor_node_name in predecessor_node_names:
-                for successor_node_name in successor_node_names:
-                    print("adding edge from", predecessor_node_name, "to", successor_node_name)
-                    G.add_edge(predecessor_node_name, successor_node_name)
-    return G, names
+            for predecessor_node_id in predecessor_node_ids:
+                for successor_node_id in successor_node_ids:
+                    print(f"{predecessor_node_id}--->{successor_node_id}")
+                    M.add_edge(predecessor_node_id, successor_node_id)
+    return M, ids
 
 
-def insert_motif(G, name, motif):
-    # get a motif
-    Gi, new_names = get_regulon(name)
-    # add the motif to the graph
-    Gn = nx.compose(G, Gi)
-    # point the prececessors of the node to replace at all nodes in first layer
-    predecessors = Gn.predecessors(name)
-    successors = new_names[0]
+def insert_motif(node_id, motif):
+    global G
+    # get a motif and add it to G
+    Gi, new_ids = get_regulon(node_id)
+    G = nx.compose(G, Gi)
+    # connect the node's predecessors to all nodes in first layer
+    predecessors = G.predecessors(node_id)
+    successors = new_ids[0]
     for predecessor in predecessors:
         for successor in successors:
             print("adding edge from", predecessor, "to", successor)
-            Gn.add_edge(predecessor, successor)
-    # point the last node in the motif at the successors of the node to replace
-    predecessors = new_names[len(new_names)-1]
-    successors = list(G.successors(name))
+            G.add_edge(predecessor, successor)
+    # connect the last layer of the motif to the node's successors
+    predecessors = new_ids[-1]
+    successors = list(G.successors(node_id))
     for predecessor in predecessors:
         for successor in successors:
             print("adding edge from", predecessor, "to", successor)
-            Gn.add_edge(predecessor, successor)
-    # remove the node
-    Gn.remove_node(name)
-    return Gn
+            G.add_edge(predecessor, successor)
+    G.remove_node(node_id)
+
+
+def recurse():
+    for step in range(STEPS):
+        nodes = G.nodes(data=True)
+        for node in nodes:
+            try:
+                if node[1]["shape"] is "square":
+                    if random.random() < P_INSERT:
+                        insert_motif(node[0], "regulon")
+            except Exception as e:
+                print('exception in recurse', e)
+        screenshot(G, f"{step+1}")
 
 
 # # we decide what boxes will do:
-# def differentiate_boxes(G):
-#     for node in G.nodes(data=True):
-#         node_id, node_data = node
-#         if node_data["shape"] is "square":
-#             layer = "dense"
-#             activation = random.choice(["linear", "tanh"])
-#             label = f"{layer} {activation}"
-#             print(f"setting {node_id} to {label}")
-#             node[1]["activation"] = activation
-#             node[1]["layer"] = layer
-#             node[1]["label"] = label
-#             node[1]["color"] = "yellow" if activation is "linear" else "green"
-#             node[1]["units"] = 64
-#             node[1]['tf_layer'] = get_layer("dense", activation=node[1]['activation'], units=node[1]['units'])
-#         if node_data["shape"] is "triangle":
-#             node[1]['activation'] = "sigmoid"
-#             node[1]['units'] = 3
-#             node[1]['tf_layer'] = get_layer("dense", activation=node[1]['activation'], units=node[1]['units'])
-#         if node_data["shape"] is "circle":
-#             node[1]["input_shape"] = (5, )
-#             node[1]['tf_layer'] = get_layer("input", shape=node[1]["input_shape"])
-#     return G
+def differentiate():
+    for node in G.nodes(data=True):
+        node_id, node_data = node
+        if node_data["shape"] is "square":
+            layer = "dense"
+            activation = random.choice(["linear", "tanh"])
+            label = f"{layer} {activation}"
+            print(f"setting {node_id} to {label}")
+            node[1]["activation"] = activation
+            node[1]["layer"] = layer
+            node[1]["label"] = label
+            node[1]["color"] = "yellow" if activation is "linear" else "green"
+            node[1]["units"] = 64
+            node[1]['tf_layer'] = get_layer("dense", activation=node[1]['activation'], units=node[1]['units'])
+        if node_data["shape"] is "triangle":
+            node[1]['activation'] = "sigmoid"
+            node[1]['units'] = 3
+            node[1]['tf_layer'] = get_layer("dense", activation=node[1]['activation'], units=node[1]['units'])
+        if node_data["shape"] is "circle":
+            node[1]["input_shape"] = (5, )
+            node[1]['tf_layer'] = get_layer("input", shape=node[1]["input_shape"])
+    return G
 
 
 def make_model():
@@ -162,8 +175,9 @@ def main():
     global G
     G = get_initial_graph(tasks)
     screenshot(G, "kamel_is_cool")
+    recurse()
     differentiate()
-    # screenshot(G2, "architecture_search_is_fun")
+    screenshot(G, STEPS + 1)
     # make_model(G2)
 
 
